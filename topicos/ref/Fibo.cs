0ec0b5abedce0e57af5056acdfa1bc36a9cadd40
Chrysthian Akihiro <chrysthian.simao@gmail.com>
2024-03-05 15:02:20 -0300

    Updated fibo and gitignore

--- topicos/ref/Fibo.cs
+++ topicos/ref/Fibo.cs
@@ -4,17 +4,28 @@ class Fibo
 {
   public static void Main(string[] args)
   {
-
     int numeroAnterior = 0;
     int numeroAtual = 1;
-    int fibonacci;
+    int fibonacci = 0;
+    int limit;
 
-    for (int i = 0; i < 10; i++)
+    Console.WriteLine("Insira o limite do fibonacci:");
+    limit = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine("----");
+    Console.WriteLine(fibonacci); //0
+    Console.WriteLine(numeroAtual); //1
+
+    while (fibonacci < limit)
     {
       fibonacci = numeroAnterior + numeroAtual;
-      Console.WriteLine(fibonacci);
-      numeroAnterior = numeroAtual;
-      numeroAtual = fibonacci;
+
+      if (fibonacci < limit)
+      {
+        Console.WriteLine(fibonacci);
+        numeroAnterior = numeroAtual;
+        numeroAtual = fibonacci;
+      }
     }
   }
 }
