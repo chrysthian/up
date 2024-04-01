82c41e2bf792d80dba84364ba3568c8bc4829f69
Chrysthian Akihiro <chrysthian.simao@gmail.com>
2024-04-01 00:24:56 -0300

    Minimal API update

--- topicos/ref/Minimal API/Models/Flavor.cs
+++ topicos/ref/Minimal API/Models/Flavor.cs
@@ -1,4 +1,15 @@
 namespace Icecream.Models
 {
-    public record Flavor(Guid Id, string Name);
+    class Flavor
+    {
+
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+
+        public Flavor(Guid Id, string Name)
+        {
+            this.Id = Id;
+            this.Name = Name;
+        }
+    }
 }
