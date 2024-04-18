be7c5decbcea162731a697a51a658dbeb58216e6
Chrysthian Akihiro <chrysthian.simao@gmail.com>
2024-04-17 21:15:17 -0300

    update day-night example

--- web avancado/next-day-night/src/app/components/Toggle/toggle.tsx
+++ web avancado/next-day-night/src/app/components/Toggle/toggle.tsx
@@ -4,10 +4,15 @@ import "./toggle.css"
 import LightModeIcon from '@mui/icons-material/LightMode';
 import ModeNightIcon from '@mui/icons-material/ModeNight';
 
+import { useDarkMode } from "@/app/state/darkModeState";
+
 const Toggle = () => {
+
+  const changeState = useDarkMode((state) => { return state.change })
+
   return (
     <div id="darkmode">
-      <input type="checkbox" className="checkbox" id="checkbox"/>
+      <input type="checkbox" className="checkbox" id="checkbox" onChange={() => { changeState() }} />
       <label htmlFor="checkbox" className="label">
         <LightModeIcon htmlColor="white" sx={{ fontSize: "12px" }} />
         <ModeNightIcon htmlColor="yellow" sx={{ fontSize: "12px" }} />
--- web avancado/next-day-night/src/app/page.tsx
+++ web avancado/next-day-night/src/app/page.tsx
@@ -1,10 +1,19 @@
 "use client"
-import React from 'react';
+import React, { useEffect } from 'react';
 import Toggle from "./components/Toggle";
 import Window from "./components/Window";
 import './page.css'
 
+import { useDarkMode } from './state/darkModeState';
+
 export default function Home() {
+
+  const isDark = useDarkMode((state) => { return state.dark })
+
+  useEffect(() => {
+    document.body.style.backgroundColor = isDark ? "#292c35" : "#fff";
+  }, [isDark]);
+
   return (
     <main>
       <Toggle />
